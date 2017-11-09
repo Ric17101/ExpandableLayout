0de1a1bf40c1baf02efc767ae8e67a4a068122bd
firstcom <firstcom@firstcoms-iMac.local>
2017-11-09 11:48:39 -0800

    Initial commit

---
+++ app/src/main/java/sg/firstcom/expandablelayout/examplesearch/ExampleSearchActivity.java
@@ -0,0 +1,59 @@
+package sg.firstcom.expandablelayout.examplesearch;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+
+import java.util.ArrayList;
+
+import sg.firstcom.expandablelayout.R;
+import sg.firstcom.expandablelayout.util.DividerItemDecoration;
+
+
+public class ExampleSearchActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static void startActivity(Context context) {
+        context.startActivity(new Intent(context, ExampleSearchActivity.class));
+    }
+
+    private Button mExpandButton;
+    private ExpandableRelativeLayout mExpandLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example_search);
+
+        getSupportActionBar().setTitle(ExampleSearchActivity.class.getSimpleName());
+
+        mExpandButton = (Button) findViewById(R.id.expandButton);
+        mExpandLayout = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout);
+        mExpandButton.setOnClickListener(this);
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final ArrayList data = new ArrayList();
+        for(int i = 0; i < 15; i++) {
+            data.add("Result");
+        }
+        recyclerView.setAdapter(new ExampleSearchRecyclerAdapter(data));
+    }
+
+    @Override
+    public void onClick(final View v) {
+        switch (v.getId()) {
+            case R.id.expandButton:
+                mExpandLayout.expand();
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/sg/firstcom/expandablelayout/expandablelayout/ExpandableLayoutActivity.java
@@ -0,0 +1,67 @@
+package sg.firstcom.expandablelayout.expandablelayout;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
+
+import sg.firstcom.expandablelayout.R;
+
+public class ExpandableLayoutActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static void startActivity(Context context) {
+        context.startActivity(new Intent(context, ExpandableLayoutActivity.class));
+    }
+
+    private Button mExpandButton;
+    private Button mMoveChildButton;
+    private Button mMoveChildButton2;
+    private Button mMoveTopButton;
+    private Button mSetCloseHeightButton;
+    private ExpandableRelativeLayout mExpandLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expandable_layout);
+
+        getSupportActionBar().setTitle(ExpandableLayoutActivity.class.getSimpleName());
+
+        mExpandButton = (Button) findViewById(R.id.expandButton);
+        mMoveChildButton = (Button) findViewById(R.id.moveChildButton);
+        mMoveChildButton2 = (Button) findViewById(R.id.moveChildButton2);
+        mMoveTopButton = (Button) findViewById(R.id.moveTopButton);
+        mSetCloseHeightButton = (Button) findViewById(R.id.setCloseHeightButton);
+        mExpandLayout = (ExpandableRelativeLayout) findViewById(R.id.expandableLayout);
+        mExpandButton.setOnClickListener(this);
+        mMoveChildButton.setOnClickListener(this);
+        mMoveChildButton2.setOnClickListener(this);
+        mMoveTopButton.setOnClickListener(this);
+        mSetCloseHeightButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View v) {
+        switch (v.getId()) {
+            case R.id.expandButton:
+                mExpandLayout.toggle();
+                break;
+            case R.id.moveChildButton:
+                mExpandLayout.moveChild(0);
+                break;
+            case R.id.moveChildButton2:
+                mExpandLayout.moveChild(1);
+                break;
+            case R.id.moveTopButton:
+                mExpandLayout.move(0);
+                break;
+            case R.id.setCloseHeightButton:
+                mExpandLayout.setClosePosition(mExpandLayout.getCurrentPosition());
+                break;
+        }
+    }
+}
